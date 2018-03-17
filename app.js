e4e2cedc7aabfeeec0e829b1cd5c95d0e153187d
rohan8594 <rohan.8594@gmail.com>
2018-03-17 10:31:28 -0700

    added app.js route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
+var search_results = require('./routes/search_results');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/about', about);
+app.use('/search_results', search_results);
 app.use('*/images',express.static('public/images'));
 
 // catch 404 and forward to error handler
