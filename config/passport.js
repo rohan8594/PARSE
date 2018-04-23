576fcf0c892b3daa42a81b6de5f3984deb4a0265
DLagos <dionmattlagos@gmail.com>
2018-04-22 20:35:19 -0700

    fixed mysql timing out error

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,17 @@
 var LocalStrategy   = require('passport-local').Strategy;
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
-var dbconfig = {
-    'connection': {
-        host: "us-cdbr-iron-east-05.cleardb.net",
-        user: "b3220b75dccc0a",
-        password: "ddd8323b"
-    },
-    'database' : "heroku_d6fcf8fd2312a32",
-    'users_table': 'user'
+
+var creds = {
+
+    host: "us-cdbr-iron-east-05.cleardb.net",
+    user: "b3220b75dccc0a",
+    password: "ddd8323b",
+    database: "heroku_d6fcf8fd2312a32"
 
 };
-var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query('USE ' + dbconfig.database);
+var connection = mysql.createPool(creds);
 
 module.exports = function(passport) {
 
@@ -30,26 +28,26 @@ module.exports = function(passport) {
     passport.use(
         'local-login',
         new LocalStrategy({
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true
-        },
-        function(req, username, password, done) {
-            connection.query("SELECT * FROM user WHERE user_id = ?",[username], function(err, rows){
-                console.log(rows[0]);
-                if (err)
-                    return done(err);
-                if (!rows.length) {
-                    return done(null, false, req.flash('loginMessage', 'Invalid Username or Password'));
-                }
+                usernameField : 'username',
+                passwordField : 'password',
+                passReqToCallback : true
+            },
+            function(req, username, password, done) {
+                connection.query("SELECT * FROM user WHERE user_id = ?",[username], function(err, rows){
+                    console.log(rows[0]);
+                    if (err)
+                        return done(err);
+                    if (!rows.length) {
+                        return done(null, false, req.flash('loginMessage', 'Invalid Username or Password'));
+                    }
 
-                // if the user is found but the password is wrong
-                if (!bcrypt.compareSync(password, rows[0].password))
-                    return done(null, false, req.flash('loginMessage', 'Invalid Username or Password.')); // create the loginMessage and save it to session as flashdata
+                    // if the user is found but the password is wrong
+                    if (!bcrypt.compareSync(password, rows[0].password))
+                        return done(null, false, req.flash('loginMessage', 'Invalid Username or Password.')); // create the loginMessage and save it to session as flashdata
 
-                // all is well, return successful user
-                return done(null, rows[0],req.flash('loginMessage', 'Welcome,' + rows[0].user_id));
-            });
-        })
+                    // all is well, return successful user
+                    return done(null, rows[0],req.flash('loginMessage', 'Welcome,' + rows[0].user_id));
+                });
+            })
     );
 };
