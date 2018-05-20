8efd78705c857e176123d9fc551fc95fdb2ddcd8
chtu <tuttle.charlie@gmail.com>
2018-05-20 16:56:50 -0700

    Updated search validation to allow anything below 6 digits

--- public/scripts/search_bar_validation.js
+++ public/scripts/search_bar_validation.js
@@ -5,7 +5,7 @@ $(function() {
 
 		if (zipcodeEntry !== "") {
 			var zipcode = parseInt(zipcodeEntry);
-			if ( isNaN(zipcode) || zipcode < 10000 || zipcode > 99999 ) {
+			if ( isNaN(zipcode) || zipcode > 99999 ) {
 				$(this).parent().find(".validation-message").html("Your zipcode must be valid.");
 				e.preventDefault();
 			}
